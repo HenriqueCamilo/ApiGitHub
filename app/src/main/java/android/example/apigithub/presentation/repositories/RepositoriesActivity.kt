63b60b3a4308cf7919920e4b6a53133dbe8727df
Henrique <henriquecamilomapa@gmail.com>
2020-09-27 13:48:54 -0300

    Modularizando

---
+++ app/src/main/java/android/example/apigithub/presentation/repositories/RepositoriesActivity.kt
@@ -0,0 +1,12 @@
+package android.example.apigithub.presentation.repositories
+
+import android.example.apigithub.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RepositoriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repositories)
+    }
+}
